69c56f2a3c990aac8d4bdf047e91952f3419710f
Thomas Jay Rush <jrush@quickblocks.io>
2024-09-01 23:20:06 -0400

    Adds version test

--- main.go
+++ main.go
@@ -1,14 +1,22 @@
 package main
 
 import (
+	"fmt"
+	"os"
 	"sync"
 
 	"github.com/TrueBlocks/trueblocks-core/src/apps/chifra/pkg/logger"
 )
 
 func main() {
+	if len(os.Args) > 1 && os.Args[1] == "--version" {
+		fmt.Println("v3.0.3-2024-09-01-23-18-10")
+		return
+	}
+
 	if config, err := establishConfig(); err != nil {
 		logger.Error(err)
+
 	} else {
 		wg := sync.WaitGroup{}
 		wg.Add(2)
