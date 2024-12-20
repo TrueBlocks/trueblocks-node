f3dd0508a1e61a15dbf7097f768e97a9acf61747
Thomas Jay Rush <jrush@quickblocks.io>
2024-12-20 11:16:03 -0500

    Move FindAvailablePort to rpc package

--- app/server.go
+++ app/server.go
@@ -2,50 +2,37 @@ package app
 
 import (
 	"fmt"
-	"net"
 	"os"
 	"strings"
 
+	"github.com/TrueBlocks/trueblocks-core/src/apps/chifra/pkg/rpc"
 	sdk "github.com/TrueBlocks/trueblocks-sdk/v4"
 )
 
 // RunServer runs the API server in a goroutine, waits for it to be ready and then
 // returns. All available trueblocks-core endpoints are available.
-func (a *App) RunServer() {
+func (a *App) RunServer() (apiUrl string, err error) {
+	apiUrl = getApiUrl()
 	ready := make(chan bool)
-	go sdk.NewDaemon(getApiUrl()).Start(ready)
+	go sdk.NewDaemon(apiUrl).Start(ready)
 	<-ready
+	return
 }
 
+// apiPort is a global variable to store the port of the API server
+// of which there can be only one.
 var apiPort = ""
 
 func getApiUrl() string {
+	// use the same port if we've already started the server...
 	if apiPort != "" {
 		return "localhost:" + apiPort
 	}
 
 	apiPort = strings.ReplaceAll(os.Getenv("TB_TEST_API_SERVER"), ":", "")
 	if apiPort == "" {
-		apiPort = fmt.Sprintf("%d", findAvailablePort())
+		apiPort = fmt.Sprintf("%d", rpc.FindAvailablePort())
 	}
-	return "localhost:" + apiPort
-}
 
-func findAvailablePort() int {
-	preferredPorts := []int{8080, 8088, 9090, 9099}
-	for _, port := range preferredPorts {
-		address := fmt.Sprintf(":%d", port)
-		listener, err := net.Listen("tcp", address)
-		if err == nil {
-			defer listener.Close()
-			return port
-		}
-	}
-	listener, err := net.Listen("tcp", ":0")
-	if err != nil {
-		return 0
-	}
-	defer listener.Close()
-	addr := listener.Addr().(*net.TCPAddr)
-	return addr.Port
+	return "localhost:" + apiPort
 }
