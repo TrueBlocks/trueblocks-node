3995c58a2c70c7c21ee1fde6eb394abc91c7c35d
Thomas Jay Rush <jrush@quickblocks.io>
2025-01-31 09:45:03 -0500

    Archived and referred to Khedra

--- config/doc.go
+++
@@ -1,4 +0,0 @@
-// Package config provides the configuration for the application. Note that most
-// configuration is provided by the underlying trueblocks-core packages, so the
-// items here are mostly for the application itself.
-package config
